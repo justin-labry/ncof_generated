31c9ec285fa968c9b4bacb50e59da117d255fac7
Yeonheon Gu <kusubang@modutech.co.kr>
2026-07-16 10:58:01 +0900

    chore(nef): remove duplicate mode scripts

--- prototype/nnef-server/run_af.sh
+++
@@ -1,9 +0,0 @@
-# 포트 단일 출처: prototype/nf_ports.conf
-set -a; . "$(dirname "$0")/../nf_ports.conf"; set +a
-export PORT=${PORT:-$NEF_PORT}
-APP_MODE=AF \
-uv run --no-sync uvicorn nnef.main:app \
-  --host 0.0.0.0 \
-  --port $PORT \
-  --log-config "./log_config.ini" \
-  --reload
--- prototype/nnef-server/run_ricf.sh
+++
@@ -1,9 +0,0 @@
-# 포트 단일 출처: prototype/nf_ports.conf (RICF 도 NEF 서버 포트를 공유)
-set -a; . "$(dirname "$0")/../nf_ports.conf"; set +a
-export PORT=${PORT:-$NEF_PORT}
-APP_MODE=RICF \
-uv run --no-sync uvicorn nnef.main:app \
-  --host 0.0.0.0 \
-  --port $PORT \
-  --log-config "./log_config.ini" \
-  --reload
